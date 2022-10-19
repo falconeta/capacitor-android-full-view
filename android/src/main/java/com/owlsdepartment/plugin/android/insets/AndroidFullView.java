09c4daca310d14e9803c1acd8f0c43d6fbc5e43c
Vittorio Lo Preiato <vittorio.lopreiato@freedompro.eu>
2022-10-19 12:01:27 +0200

    add light navigation bar flag

--- android/src/main/java/com/owlsdepartment/plugin/android/insets/AndroidFullView.java
+++ android/src/main/java/com/owlsdepartment/plugin/android/insets/AndroidFullView.java
@@ -2,7 +2,6 @@ package com.falconeta.plugin.android.fullview;
 
 import android.util.DisplayMetrics;
 import android.view.View;
-
 import androidx.appcompat.app.AppCompatActivity;
 
 public class AndroidFullView {
@@ -11,9 +10,15 @@ public class AndroidFullView {
 
     public AndroidFullView(AppCompatActivity activity) {
         this.activity = activity;
-      activity.getWindow().getDecorView().setSystemUiVisibility(
-        View.SYSTEM_UI_FLAG_LAYOUT_STABLE
-          | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION);
+        activity
+            .getWindow()
+            .getDecorView()
+            .setSystemUiVisibility(
+                View.SYSTEM_UI_FLAG_LAYOUT_STABLE |
+                View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION |
+                View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR |
+                View.SYSTEM_UI_FLAG_LIGHT_NAVIGATION_BAR
+            );
     }
 
     public float getTop() {
